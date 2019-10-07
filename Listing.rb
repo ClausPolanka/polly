911a5dfdb01cbb448f3bf1b710a82f8973371f95
polacla1 <claus.polanka@chello.at>
2019-10-07 20:00:48 +0200

    State of code after change 3 of Sandi's code (#0 => #...)

--- Listing.rb
+++ Listing.rb
@@ -33,13 +33,18 @@ class Listing
     File.read("code/code.rb").split("\n")
   end
 
-  # "1, 3-4, 15, 37-50"
+  # "1, #4, 3-4, #6, 15, 37-50"
   def lines_to_print(all_lines)
     specs = line_numbers.gsub(/[‘|’]/, "").gsub(/ /, '').split(",")
     specs.collect { |spec|
-      edges = spec.split('-').collect(&:to_i)
-      individiual_numbers = (edges.min.to_i..edges.max.to_i).to_a
-      individiual_numbers.collect { |i| all_lines[i - 1] }.compact
-    }.flatten.compact
+      if spec.include?('#')
+        num_spaces = spec.delete('#').to_i
+        (" " * num_spaces) + "# ..."
+      else
+        edges = spec.split('-').collect(&:to_i)
+        individiual_numbers = (edges.min.to_i..edges.max.to_i).to_a
+        individiual_numbers.collect { |i| all_lines[i - 1] }.compact
+      end
+  }.flatten.compact
   end
 end
--- main.rb
+++ main.rb
@@ -3,7 +3,7 @@ require_relative 'Listing'
 
 listing = Listing.new(
   filename: "code.rb",
-  line_numbers: "2-3, 1",
+  line_numbers: "#0, 2-3, #0, 1",
   repository: "code/.git",
   tag: "test_version",
   git_cmd: GitCmd.new)
